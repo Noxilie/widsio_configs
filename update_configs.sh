bc6be5065068adbd1eace251b04ba64844364a9e
Noxilie <noxilie@gmail.com>
2026-07-05 23:19:28 +0200

    fix whitespaces in modIds

--- update_configs.sh
+++ update_configs.sh
@@ -2,7 +2,7 @@
 
 CONF_FILE="widsio.ini"
 
-wids="$(awk '{print $1}' mod_list.txt | uniq | tr '\n' ';' | sed 's/&/\\&/g' | sed 's/;$//')"
-mids="$(awk '{print $2}' mod_list.txt | uniq | tr '\n' ';' | sed 's/&/\\&/g' | sed 's/;$//')"
+wids="$(awk -F';' '{print $1}' mod_list.txt | uniq | tr '\n' ';' | sed 's/;$//')"
+mids="$(awk -F';' '{print $2}' mod_list.txt | sed 's/&/\\&/g' | tr '\n' ';' | sed 's/;$//')"
 
 sed -i.bak -e "s|^Mods=.*|Mods=$mids|" -e "s|^WorkshopItems=.*|WorkshopItems=$wids|" $CONF_FILE
