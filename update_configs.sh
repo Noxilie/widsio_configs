9457164d3a64b2ea285782122eb585c64fe04b61
Noxilie <noxilie@gmail.com>
2026-07-06 17:08:01 +0200

    oops, forgot about the default map -_-

--- update_configs.sh
+++ update_configs.sh
@@ -5,6 +5,6 @@ CONF_FILE="widsio.ini"
 
 wids="$(awk -F';' '{print $1}' $LIST_FILE | grep -ve '^$' | uniq | tr '\n' ';' | sed 's/;$//')"
 mids="$(awk -F';' '{print $2}' $LIST_FILE | grep -ve '^$' | sed 's/&/\\&/g' | tr '\n' ';' | sed 's/;$//')"
-maps="$(awk -F';' '{print $3}' $LIST_FILE | grep -ve '^$' | sed 's/&/\\&/g' | tr '\n' ';' | sed 's/;$//')"
+maps="$(awk -F';' '{print $3}' $LIST_FILE | grep -ve '^$' | sed 's/&/\\&/g' | tr '\n' ';')Muldraugh, KY"
 
 sed -i.bak -e "s|^Mods=.*|Mods=$mids|" -e "s|^Map=.*|Map=$maps|" -e "s|^WorkshopItems=.*|WorkshopItems=$wids|" $CONF_FILE
