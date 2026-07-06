8303e7fff9c3389c42db77a5da826e103c7a3dab
Noxilie <noxilie@gmail.com>
2026-07-06 16:47:47 +0200

    add maps to mod_list file

--- update_configs.sh
+++ update_configs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+LIST_FILE="mod_list.txt"
 CONF_FILE="widsio.ini"
 
-wids="$(awk -F';' '{print $1}' mod_list.txt | uniq | tr '\n' ';' | sed 's/;$//')"
-mids="$(awk -F';' '{print $2}' mod_list.txt | sed 's/&/\\&/g' | tr '\n' ';' | sed 's/;$//')"
+wids="$(awk -F';' '{print $1}' $LIST_FILE | grep -ve '^$' | uniq | tr '\n' ';' | sed 's/;$//')"
+mids="$(awk -F';' '{print $2}' $LIST_FILE | grep -ve '^$' | sed 's/&/\\&/g' | tr '\n' ';' | sed 's/;$//')"
+maps="$(awk -F';' '{print $3}' $LIST_FILE | grep -ve '^$' | sed 's/&/\\&/g' | tr '\n' ';' | sed 's/;$//')"
 
-sed -i.bak -e "s|^Mods=.*|Mods=$mids|" -e "s|^WorkshopItems=.*|WorkshopItems=$wids|" $CONF_FILE
+sed -i.bak -e "s|^Mods=.*|Mods=$mids|" -e "s|^Map=.*|Map=$maps|" -e "s|^WorkshopItems=.*|WorkshopItems=$wids|" $CONF_FILE
